b845dda8e9c6d937580f973d685f6a195efcbf28
Zach Piddock <zpiddock@gmail.com>
2024-03-10 19:31:22 +0000

    Updates

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,35 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, RouterProvider, createBrowserRouter, Routes as ReactRoutes, Route } from 'react-router-dom';
+import Routes from './routes';
+import Notes from './notes';
+
+const router = createBrowserRouter([
+{
+  path: "/",
+  element: <App />,
+},
+{
+  path: "/notes",
+  element: <Notes />
+}
+])
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <Routes/> */}
+    {/* <RouterProvider router={router} /> */}
+    <BrowserRouter>
+      <Routes/>
+      <ReactRoutes>
+        <Route path='/' element={<App/>}/>
+        <Route path='/notes' element={<Notes/>}/>
+      </ReactRoutes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
---
+++ src/notes.tsx
@@ -0,0 +1,15 @@
+import { TextareaAutosize } from "@mui/base"
+import Editor from "@react-page/editor"
+import "./notes.css"
+
+export default function Notes() {
+
+    return (
+        <>
+        <div id="content">
+            <label> Notes</label>
+            <textarea/>
+        </div>
+        </>
+    )
+}
